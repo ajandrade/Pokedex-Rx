74b66dbbc44a33dbf2cd29a0d2d33309c599c4a8
Amadeu Andrade <amadeu.dev@gmail.com>
2017-09-17 16:30:43 +0100

    Minor UI update on pokedex list

--- Pokedex-Rx/PokedexListViewController.swift
+++ Pokedex-Rx/PokedexListViewController.swift
@@ -14,10 +14,26 @@ class PokedexListViewController: UIViewController {
   
   var viewModel: PokedexListViewModelRepresentable!
   
+  // MARK: - IBOUTLETS
+  
+  @IBOutlet private weak var collectionView: UICollectionView!
+  @IBOutlet private weak var musicButton: UIButton!
+  @IBOutlet private weak var searchBar: UISearchBar!
+  
   // MARK: - VIEW LIFE CYCLE
   
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+}
+
+extension PokedexListViewController: UICollectionViewDelegateFlowLayout {
+  
+  // swiftlint:disable line_length
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 105.0, height: 105.0)
+  }
+  // swiftlint:enable line_length
   
 }
