f087d4cc5cec04347def9f6d4323bc91c3f391ff
Amadeu Andrade <amadeu.dev@gmail.com>
2017-09-19 19:18:49 +0100

    Created PokemonService file

---
+++ Pokedex-Rx/PokemonService.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonService.swift
+//  Pokedex-Rx
+//
+//  Created by Amadeu Andrade on 17/09/2017.
+//  Copyright © 2017 Amadeu Andrade. All rights reserved.
+//
+
+import Foundation
+
+protocol PokemonNetworkProtocol {
+  
+}
+
+struct PokemonNetwork: PokemonNetworkProtocol {
+
+}
